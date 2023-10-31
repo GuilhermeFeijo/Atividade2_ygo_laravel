a4ead4aed4d28a4617c1aedeae605caaf9f2d120
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2023-10-31 08:35:38 -0300

    feat: autenticação adicionada

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tickets;
 use Illuminate\Http\Request;
 
 class TicketsController extends Controller
 {
-    //
+    public function index()
+    {
+        $tickets = Tickets::all(); //Coleta do banco todos os campos do ticket
+
+        return view('admin.tickets.index', compact('tickets')); //compact -> passa um array com tudo que contém no tickets
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Rotas Tickets
+
+//Rotas Admin
+  //Tickets
 Route::get('/', function () {
-    return view('tickets');
+    return view('welcome');
 });
 
-//Rotas Dominios
 
 
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
